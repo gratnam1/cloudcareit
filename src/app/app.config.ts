bd9f7c5cacf47863ca463089e251d78044551779
Gaj Ratnam <gratnam@paymentus.com>
2026-02-08 08:13:47 -0500

    update to ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     // withComponentInputBinding allows us to pass Route Data to components easily
     provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
-    provideClientHydration()
+    provideClientHydration(), provideClientHydration(withEventReplay())
   ]
 };
